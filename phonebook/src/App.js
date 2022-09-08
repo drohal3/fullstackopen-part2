13122ed174b0ff94357f8ef62eeaf94f35c00b1f
Dominik Rohal <rohal.dominik.123@gmail.com>
2022-09-08 15:56:11 +0300

    Exercise 2.10 solution and cleanup

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,11 +1,45 @@
 import {useState} from 'react'
 
+const Filter = ({filter, changeHandler}) => {
+    return (
+        <form>
+            <div>
+                filter shown with: <input value={filter} onChange={changeHandler}/>
+            </div>
+        </form>
+    )
+}
+
+const PersonForm = ({name, nameChangeHandler, number, numberChangeHandler, addPersonClickHandler}) => {
+    return (
+        <form>
+            <div>
+                name: <input value={name} onChange={nameChangeHandler}/>
+            </div>
+            <div>
+                number: <input value={number} onChange={numberChangeHandler}/>
+            </div>
+            <div>
+                <button type="submit" onClick={addPersonClickHandler}>add</button>
+            </div>
+        </form>
+    )
+}
+
+const Persons = ({persons}) => {
+    return (
+        persons.map(person =>
+            <p key={person.name}>{person.name} {person.number}</p>
+        )
+    )
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
@@ -49,27 +83,12 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
-                <div>
-                    filter shown with: <input value={filter} onChange={handleFilterChange}/>
-                </div>
-            </form>
+            <Filter filter={filter} changeHandler={handleFilterChange}/>
             <h2>Add new</h2>
-            <form>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={handleNumberChange}/>
-                </div>
-                <div>
-                    <button type="submit" onClick={addPerson}>add</button>
-                </div>
-            </form>
+            <PersonForm name={newName} nameChangeHandler={handleNameChange} number={newNumber}
+                        numberChangeHandler={handleNumberChange} addPersonClickHandler={addPerson}/>
             <h2>Numbers</h2>
-            {personsToShow.map(person =>
-                <p key={person.name}>{person.name} {person.number}</p>
-            )}
+            <Persons persons={personsToShow}/>
         </div>
     )
 }
